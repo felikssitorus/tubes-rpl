8889b7e580f0e64142cde2c10f44caab9ae43a2d
BerlianGita <berliangitaayu04@gmail.com>
2025-12-02 23:24:39 +0700

    Modified App.jsx (tambah mhs)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import { Router, Route } from "@solidjs/router";
+import { Router, Routes, Route } from "@solidjs/router";
 
 import EmployeePage from "./pages/employees/EmployeePage";
 import KomponenPage from "./pages/komponen/KomponenPage";
+import LoginPage from "./components/mahasiswa/login/LoginPage";
+import Dashboard from "./components/mahasiswa/dashboard/Dashboard";
 
 export default function App() {
   return (
     <Router>
-      <Route path="/" component={EmployeePage} />
-      <Route path="/komponen" component={KomponenPage} />
+      <Routes>
+        <Route path="/" component={LoginPage} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/employees" component={EmployeePage} />
+        <Route path="/komponen" component={KomponenPage} />
+      </Routes>
     </Router>
-
-    
   );
 }
