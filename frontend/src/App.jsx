369f6938c3ac6b4f90a06dd984cfc48f62ed1f54
FeliksMindoTuaSitorus <felikssitorus@gmail.com>
2025-12-10 14:01:48 +0700

    feat: add rubrik page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import DashboardPage from "./pages/dashboard/dashboardPage.jsx";
 import MenuPage from "./pages/menu/menuPage.jsx";
 import KelompokPage from "./pages/kelompok/kelompokPage.jsx";
 import KomponenPage from "./pages/komponen/KomponenPage.jsx";
+import RubrikPage from "./pages/rubrik/rubrikPage.jsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Route path="/menu/:courseCode" component={MenuPage} />
       <Route path="/kelompok/:courseId" component={KelompokPage} />
       <Route path="/komponen" component={KomponenPage} />
+      <Route path="/rubrik" component={RubrikPage} />
     </Router>
   );
 }
